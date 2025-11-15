7a2869bac62933efbc0d334ef064bbb9d8ad07c0
JiniusW <jsw2855264@gmail.com>
2025-11-15 17:39:01 +0900

    +++ numpy compibility on bbox.pyx

--- lib/fpn/box_intersections_cpu/bbox.pyx
+++ lib/fpn/box_intersections_cpu/bbox.pyx
@@ -21,8 +21,8 @@ try:
 except AttributeError:
     np.bool8 = bool
 
-DTYPE = np.float
-ctypedef np.float_t DTYPE_t
+DTYPE = np.64float
+ctypedef np.64float_t DTYPE_t
 
 def bbox_overlaps(boxes, query_boxes):
     cdef np.ndarray[DTYPE_t, ndim=2] boxes_contig = np.ascontiguousarray(boxes, dtype=DTYPE)
